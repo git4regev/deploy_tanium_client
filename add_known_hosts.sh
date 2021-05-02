d91bf5f099a0ab33e5d5b5040ad89e923ea382a5
git4regev <54402954+git4regev@users.noreply.github.com>
2021-05-01 21:58:54 -0400

    Update add_known_hosts.sh

--- add_known_hosts.sh
+++ add_known_hosts.sh
@@ -1,5 +1,3 @@
-for i in $(cat ansible_hosts)
-do
-ssh-keyscan $i >> ~/.ssh/known_hosts
-done
-
+while IFS=$'\t' read -r -a tmp ; do
+ssh-keyscan "${tmp[0]}" >> ~/.ssh/known_hosts
+done < ansible_hosts
