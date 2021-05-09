75279dc8f90cac48eb248fc447f0b9369f3f7267
git4regev <54402954+git4regev@users.noreply.github.com>
2021-05-09 18:21:52 -0400

    Add files via upload

--- configure_ssh_authentication.py
+++ configure_ssh_authentication.py
@@ -24,49 +24,73 @@ hosts_keys_folder = 'hosts_authentication'
 
 ansible_key = open(os.path.expanduser('~/.ssh/id_rsa.pub')).read()
 
+print ("configure_ssh_authentication")
+print ("============================\n")
+
 # Create known_hosts file if one does not already exists
+print ("* Checking existance of file {}...".format(known_hosts))
 if not os.path.isfile(known_hosts):
     open(known_hosts, 'w+').close()
 
 with open(ansible_file) as csv_file:
-    print ("Reading content of {}...".format(ansible_file))
-    csv_reader = csv.reader(csv_file, delimiter=' ')
+    print ("* Reading content of file {}...".format(ansible_file))
+    try:
+        csv_reader = csv.reader(csv_file, delimiter=' ')
+    except:
+        print ("\t- ERROR: Could not open file {}. Aborting script.".format(csv_file))
+        exit()
     for row in csv_reader:
-        try:
-            if len(row)>0:
-                host = row[0]
-                if host[0]!='[' and host[0]!='#' and ignore_line!=True:
-                    # Find username for host from ansible hosts file
-                    host_username = ""
-                    for item in row:
-                        if "ansible_user" in item:
-                            host_username = (item.split("="))[1]
-                    # Adding the fingerprint from a remote server to a local machine
-                    print ("Adding fingerpring for host {} to Ansible host...".format(host))
-                    address = host+':22'
+        if len(row)>0:
+            host = row[0]
+            if host[0]!='[' and host[0]!='#' and ignore_line!=True:
+                print ("-----------------------")
+                print ("* Configuring SSH authentication for host {}...".format(host))
+                # Find username for host from ansible hosts file
+                host_username = ""
+                for item in row:
+                    if "ansible_user" in item:
+                        host_username = (item.split("="))[1]
+                        print ("\t- Found username {}.".format(host_username))
+                    else:
+                        print ("\t\t- WARNING: no username defined in Ansible hosts file for host {}.".format(host))
+                # Adding the fingerprint from a remote server to a local machine
+                print ("\t- Getting public key from host {}...".format(host))
+                address = host+':22'
+                try:
                     transport = paramiko.Transport(host,'22')
                     transport.connect()
                     key = transport.get_remote_server_key()
                     transport.close()
-                    hostfile = paramiko.HostKeys(filename=known_hosts)
-                    hostfile.add(hostname = host, key=key, keytype=key.get_name())
-                    hostfile.save(filename=known_hosts)
-                    # Add public key of local host to remote server
-                    if (host_username != ""):
-                        print ("Adding Ansible public key to remote host {}...".format(host))
-                        remote_server_key_file = os.path.dirname(os.path.realpath(__file__)) + "/" + hosts_keys_folder + "/" + host + ".pem"
-                        os.chmod(remote_server_key_file, 0o600)
-                        remote_server_key = paramiko.RSAKey.from_private_key_file(remote_server_key_file)
-                        client = paramiko.SSHClient()
-                        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                        #client.connect(server, username=username, password=password)
-                        client.connect( hostname = host, username = host_username, pkey = remote_server_key )
-                        client.exec_command('echo "%s" > ~/.ssh/authorized_keys' % ansible_key)
-                        client.close()
-                elif host[0]=='[':
-                    if host=='[windows_endpoints]':
-                        ignore_line = True
+                except:
+                    print ("\t- ERROR: Could not retrieve public key from host.")
+                else:
+                    try:
+                        print ("\t- Storing public key on local host...")
+                        hostfile = paramiko.HostKeys(filename=known_hosts)
+                        hostfile.add(hostname = host, key=key, keytype=key.get_name())
+                        hostfile.save(filename=known_hosts)
+                    except:
+                        print ("\t- ERROR: Could not save public key on localhost.")
                     else:
-                        ignore_line = False
-        except:
-            print ('service not known for host '+address)
+                        # Add public key of local host to remote server
+                        if (host_username != ""):
+                            print ("\t- Adding localhost public key to remote host {}...".format(host))
+                            try:
+                                remote_server_key_file = os.path.dirname(os.path.realpath(__file__)) + "/" + hosts_keys_folder + "/" + host + ".pem"
+                                os.chmod(remote_server_key_file, 0o600)
+                                remote_server_key = paramiko.RSAKey.from_private_key_file(remote_server_key_file)
+                                client = paramiko.SSHClient()
+                                client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                                #client.connect(server, username=username, password=password)
+                                client.connect( hostname = host, username = host_username, pkey = remote_server_key )
+                                client.exec_command('echo "%s" > ~/.ssh/authorized_keys' % ansible_key)
+                                client.close()
+                            except:
+                                print ("\t- ERROR: Could not add local host public key to remote host.")
+                            else:
+                                print ("\t- SUCCESS. SSH authentication configuration for host {} is complete.".format(host)))
+            elif host[0]=='[':
+                if host=='[windows_endpoints]':
+                    ignore_line = True
+                else:
+                    ignore_line = False
