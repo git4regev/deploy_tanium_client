4fe79cac1189dcd821124818e8b45838dd163752
git4regev <54402954+git4regev@users.noreply.github.com>
2021-05-12 09:15:13 -0400

    Add files via upload

--- configure_ssh_authentication.py
+++ configure_ssh_authentication.py
@@ -84,9 +84,11 @@ with open(ansible_file) as csv_file:
                                     remote_server_key = paramiko.RSAKey.from_private_key_file(remote_server_key_file)
                                     client = paramiko.SSHClient()
                                     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                                    #client.connect(server, username=username, password=password)
                                     client.connect( hostname = host, username = host_username, pkey = remote_server_key )
+                                    client.exec_command('mkdir -p ~/.ssh/')
                                     client.exec_command('echo "%s" > ~/.ssh/authorized_keys' % ansible_key)
+                                    client.exec_command('chmod 644 ~/.ssh/authorized_keys')
+                                    client.exec_command('chmod 700 ~/.ssh/')
                                     client.close()
                                 elif os.path.isfile(remote_server_password_file):
                                     print ("\t- Found .txt file for remote host. Using username/password for authentication...")
@@ -96,7 +98,10 @@ with open(ansible_file) as csv_file:
                                     client = paramiko.SSHClient()
                                     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
                                     client.connect(host, username=host_username, password=host_password)
+                                    client.exec_command('mkdir -p ~/.ssh/')
                                     client.exec_command('echo "%s" > ~/.ssh/authorized_keys' % ansible_key)
+                                    client.exec_command('chmod 644 ~/.ssh/authorized_keys')
+                                    client.exec_command('chmod 700 ~/.ssh/')
                                     client.close()                                
                             except Exception as e:
                                 print ("\t- ERROR: Could not add local host public key to remote host.")
